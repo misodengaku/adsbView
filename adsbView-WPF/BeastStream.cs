d3a7e7c039ccad1e4ccb4ef8c5ac5041a99bc051
misodengaku <misodengaku@gmail.com>
2014-02-01 16:41:32 +0900

    first commit

---
+++ adsbView-WPF/BeastStream.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adsbView_WPF
+{
+	public class BeastData
+	{
+		TcpClient client;
+		Stream tcpStream;
+		bool IsOpened;
+		//byte[] dataBuffer = new byte[1024];
+		List<byte> dataBuffer = new List<byte>();
+
+		public BeastData(string host, int port)
+		{
+			client = new TcpClient(host, port);
+			tcpStream = client.GetStream();
+			IsOpened = true;
+		}
+
+		/// <summary>
+		/// Beast バイナリーフォーマットの1パケット分を読み出します。
+		/// </summary>
+		/// <returns></returns>
+		public bool GetByteLine(byte[] retData)
+		{
+			if (dataBuffer.Count == 0)
+			{
+				while (tcpStream.ReadByte() != 0x1a) ; //頭出し
+				dataBuffer.Add(0x1a);
+			}
+
+			while (client.Available > 1)
+			{
+				var data = (byte)tcpStream.ReadByte();
+				dataBuffer.Add(data);
+				if (data == 0x1a)
+				{
+					dataBuffer.CopyTo(0, retData, 0, dataBuffer.Count - 1);
+					dataBuffer.Clear();
+					dataBuffer.Add(0x1a);
+					return true;
+				}
+			}
+			return false;
+			
+		}
+	}
+}
---
+++ adsbView-WPF/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace adsbView_WPF
+{
+	/// <summary>
+	/// MainWindow.xaml の相互作用ロジック
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			Task.Factory.StartNew(() =>
+			{
+				var bs = new BeastData("localhost", 31001);
+				while (true) {
+					var data = new byte[24];
+					if (bs.GetByteLine(data)){
+						string line = "";
+						foreach (var x in data)
+						{
+							if (x == 0)
+								break;
+							line += x.ToString("x2") + " ";
+						}
+						this.Dispatcher.BeginInvoke(new Action(() =>
+						{
+							adsbPacketBox.Items.Add(line);
+						}));
+					}
+					Thread.Sleep(100);
+				}
+			});
+
+
+		}
+
+	}
+}
